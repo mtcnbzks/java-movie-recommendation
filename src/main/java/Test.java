c0817cf4925cfe8b231c7139fbd65c291277b0df
mtcnbzks <mtcnbzks@gmail.com>
2023-06-01 23:54:49 +0300

    test zone deleted and excluded from repo

--- src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class Test {
-   public static void main(String[] args) {
-      List<Movie> movies = Recommender.getMovies();
-
-      System.out.println(movies.size());
-   }
-}
